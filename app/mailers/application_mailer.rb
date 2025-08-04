1e0e96f6550697c420feb29428ca96db4c1c9c9f
YCC <yccopac@ucenfotec.ac.cr>
2025-08-03 20:31:57 -0600

    Add custom mail template.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+class ApplicationMailer < Devise::Mailer
+  helper :application # gives access to `application_helper`
+  default template_path: 'mailer' # make sure this folder exists
+
+  def confirmation_instructions(record, token, opts = {})
+    opts[:subject] = "Welcome! Confirm Your Email"
+    super
+  end
 end
