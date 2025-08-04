2a5688ba406fb53d47a07b33d1f442337bfdacd4
YCC <yccopac@ucenfotec.ac.cr>
2025-08-03 22:37:12 -0600

    Config home test.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(
+      email: "test@example.com",
+      password: "password123",
+      uid: SecureRandom.uuid,
+      provider: "email"
+    )
+  end
+
   test "should get index" do
-    get home_index_url
+    get root_url
     assert_response :success
   end
 end
