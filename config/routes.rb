36589ebbfa068688da4c62beb70c1143496cfdba
YCC <yccopac@ucenfotec.ac.cr>
2025-08-15 20:44:45 -0600

    Fix: Magic login not sending mail.

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,26 @@ Rails.application.routes.draw do
         registrations: "api/v1/auth/registrations",
         sessions: "api/v1/auth/sessions"
       }
-      get '/magic_login', to: 'auth/sessions#magic_login', as: :magic_login
+      devise_scope :user do
+        get 'magic_login', to: 'auth/sessions#magic_login'
+      end
 
-      resources :user_roles, only: [ :create, :update, :destroy ]
+      resources :user_roles, only: [:create, :update, :destroy]
+
       resources :denied_accesses, only: [] do
         collection do
           patch :toggle_visibility
         end
       end
+
       resources :users
-      resources :areas, only: [ :index, :create, :show, :update, :destroy ]
-      resources :shelves, only: [ :index, :create, :show, :update, :destroy ]
-      resources :containers, only: [ :index, :create, :show, :update, :destroy ]
+      resources :areas,      only: [:index, :create, :show, :update, :destroy]
+      resources :shelves,    only: [:index, :create, :show, :update, :destroy]
+      resources :containers, only: [:index, :create, :show, :update, :destroy]
     end
   end
 
-  # Let React Router handle all frontend routes
-  get "*path", to: "home#index", constraints: ->(req) { !req.xhr? && req.format.html? && !req.path.starts_with?("/api") }
+  get "*path", to: "home#index", constraints: ->(req) {
+    !req.xhr? && req.format.html? && !req.path.starts_with?("/api")
+  }
 end
